d573b57097ac99b20a5b05abbe8bb527ecd5decd
Josh Blood <jblood92@pm.me>
2023-07-27 11:59:55 -0400

    Form is pushing to todos, toggles working

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,12 +1,33 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      title: e.target.value
+    })
+  }
+
+  pushTodo = (e) => {
+    e.preventDefault();
+    this.props.addTodo(e, this.state.title);
+    this.setState({
+      title: ''
+    })
+  }
+
   render() {
     return (
       <div>
-        <form onSubmit={this.props.addTodo}>
-          <input type="text" placeholder="Add Todo" />
-          <button>Add Todo</button>
+        <form>
+          <input type="text" placeholder="Add Todo" onChange={this.handleChange}/>
+          <button onClick={this.pushTodo}>Add Todo</button>
         </form>
       </div>
     )
